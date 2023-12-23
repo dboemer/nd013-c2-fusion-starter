02bf79975c38b2d9283f18b7860ed1beed17a8dc
dboemer <dominik.boemer@gmail.com>
2023-12-23 15:28:04 +0000

    exercise ID_S2_EX2 coded

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -126,10 +126,10 @@ def bev_from_pcl(lidar_pcl, configs):
     #######
     print("student task ID_S2_EX1")
 
-    ## step 1 : compute bev-map discretization by dividing x-range by the bev-image height (see configs)
+    # step 1 : compute bev-map discretization by dividing x-range by the bev-image height (see configs)
     bev_discret = (configs.lim_x[1] - configs.lim_x[0]) / configs.bev_height
 
-    ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates
+    # step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates
     lidar_pcl_cpy = np.copy(lidar_pcl)
     lidar_pcl_cpy[:, 0] = np.int_(np.floor(lidar_pcl_cpy[:, 0] / bev_discret))
 
@@ -148,19 +148,31 @@ def bev_from_pcl(lidar_pcl, configs):
     #######
     print("student task ID_S2_EX2")
 
-    ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
-
+    # step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    intensity_map = np.zeros((configs.bev_height, configs.bev_width))
+    
     # step 2 : re-arrange elements in lidar_pcl_cpy by sorting first by x, then y, then -z (use numpy.lexsort)
+    lidar_pcl_cpy[lidar_pcl_cpy[:, 3]>1.0, 3] = 1.0
+    idx_intensity = np.lexsort((-lidar_pcl_cpy[:, 2], lidar_pcl_cpy[:, 1], lidar_pcl_cpy[:, 0]))
+    lidar_pcl_cpy = lidar_pcl_cpy[idx_intensity]
 
-    ## step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
-    ##          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
+    # step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
+    #          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
+    _, indices, counts = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True, return_counts=True)
+    lidar_top_pcl = lidar_pcl_cpy[indices]
+    
+    # step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
+    #          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
+    #          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
+    intensity_map[np.int_(lidar_top_pcl[:, 0]), np.int_(lidar_top_pcl[:, 1])] = lidar_top_pcl[:, 3]/(np.amax(lidar_top_pcl[:, 3]) - np.amin(lidar_top_pcl[:, 3]))
 
-    ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
-    ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
-    ##          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
-
-    ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
-
+    # step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
+    img_intensity = intensity_map * 256
+    img_intensity = img_intensity.astype(np.uint8)
+    cv2.imshow('img_intensity', img_intensity)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
     #######
     ####### ID_S2_EX2 END ####### 
 
