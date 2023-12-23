eb6a1530847c2b16f2d72dec8239d59c310fa565
dboemer <dominik.boemer@gmail.com>
2023-12-23 12:13:07 +0000

    exercise ID_S1_EX2 coded

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -14,6 +14,7 @@
 import cv2
 import numpy as np
 import torch
+import open3d as o3d
 
 # add project directory to python path to enable relative imports
 import os
@@ -39,14 +40,26 @@ def show_pcl(pcl):
     print("student task ID_S1_EX2")
 
     # step 1 : initialize open3d with key callback and create window
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window(window_name='Open3D', width=1280, height=720)
+    vis.get_render_option().point_size = 2
     
     # step 2 : create instance of open3d point-cloud class
+    pcd = o3d.geometry.PointCloud()
 
     # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
+    pcd.points = o3d.utility.Vector3dVector(pcl[:, :3])
 
     # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
+    vis.add_geometry(pcd)
     
     # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
+    def key_callback(vis):
+        vis.close()
+        return False
+    
+    vis.register_key_callback(262, key_callback)
+    vis.run()
 
     #######
     ####### ID_S1_EX2 END #######     
